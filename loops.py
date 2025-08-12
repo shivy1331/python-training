d200340eac51be05ba56dc7cc05f19f7c3293979
shivendra singh <shivendrasingh1331@gmail.com>
2025-08-12 16:12:53 +0530

    factorial of a number

--- loops.py
+++ loops.py
@@ -75,10 +75,20 @@
 # words_length(["Hello", "World", "Python", "Programming"])
 
 # else clause in for loop
-for i in range(5):
-    if i == 3:
-        print("Breaking the loop at i =", i)
-        break
-    print(i)
-else:
-    print("Loop completed without break")  # This will execute after the loop finishes normally
+# for i in range(5):
+#     if i == 3:
+#         print("Breaking the loop at i =", i)
+#         break
+#     print(i)
+# else:
+#     print("Loop completed without break")
+
+# factorial of a number using for loop
+num = int(input("Enter a number: "))
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result 
+
+print("Factorial of", num, "is:", factorial(num))
