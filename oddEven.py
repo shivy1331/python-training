b0886f98854dd2329328b638d8a581c1c2ea3774
shivendra singh <shivendrasingh1331@gmail.com>
2025-08-12 10:46:37 +0530

    new commit

--- oddEven.py
+++ oddEven.py
@@ -1,5 +1,5 @@
-x = 5
-if x%2 == 0:
-    print(x ,"is Even number.")
+x = int(input("Enter a number: "))  # convert to integer
+if x % 2 == 0:
+    print(x, "is Even number.")
 else:
     print(x, "is Odd number.")
