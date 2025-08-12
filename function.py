6ca1d682f503a9448eabb42023ee2b1d50986fbb
shivendra singh <shivendrasingh1331@gmail.com>
2025-08-12 11:50:01 +0530

    function declaration

---
+++ function.py
@@ -0,0 +1,9 @@
+# function declaration
+def fun1():
+    print("Function 1 executed")
+# function calling
+fun1()
+fun1()
+fun1()
+fun1()
+fun1()
--- oddEven.py
+++ oddEven.py
@@ -2,4 +2,4 @@ x = int(input("Enter a number: "))  # convert to integer
 if x % 2 == 0:
     print(x, "is Even number.")
 else:
-    print(x, "is Odd number.")
+    print(x, "is Odd number.") 
