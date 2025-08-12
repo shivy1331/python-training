2c03f80faaaf6e4b2ad582172373000f250621c0
shivendra singh <shivendrasingh1331@gmail.com>
2025-08-12 16:20:02 +0530

    Oops in python

---
+++ OopsInPython.py
@@ -0,0 +1,15 @@
+# oops
+# Oops is programming paradigm that structures code around ob
+
+# classes ->
+# class is a blueprint for creating objects
+
+# objects ->
+# object is an instance of a class
+
+# methods ->
+# method is a function defined inside a class
+
+# variables/attributes ->
+# variable is a named storage for data
+# attribute is a variable that belongs to an object
