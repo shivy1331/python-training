f298354ab69760c93909b7cead0c1d38f668db0f
shivendra singh <shivendrasingh1331@gmail.com>
2025-08-12 11:59:19 +0530

    multiplication

--- function.py
+++ function.py
@@ -7,4 +7,11 @@ fun1()
 # function with parameters
 def add(a, b):
     return a + b
-print("Addition of a+b is: ",add(5, 10))
+print("Addition of a+b is:",add(5, 10))
+
+# take input from user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+def multiply(x, y):
+    return x * y 
+print("Multiplication of a*b is:", multiply(a, b))
