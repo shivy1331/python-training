8a63fd89b9691335a9cdabdf6919930b0b9ad626
shivendra singh <shivendrasingh1331@gmail.com>
2025-08-12 11:56:56 +0530

    function with parameters

--- function.py
+++ function.py
@@ -3,7 +3,8 @@ def fun1():
     print("Function 1 executed")
 # function calling
 fun1()
-fun1()
-fun1()
-fun1()
-fun1()
+
+# function with parameters
+def add(a, b):
+    return a + b
+print("Addition of a+b is: ",add(5, 10))
