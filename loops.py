e538bddf79a395022da86b422e64a7bb529d7ed7
shivendra singh <shivendrasingh1331@gmail.com>
2025-08-12 13:27:31 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,18 @@
+# there are two types of loops in Python:
+
+# 1. for loop
+# 2. while loop
+
+# for loop
+# for i in range(5):
+#     print(i, end=' ')
+
+# fruits = ['apple', 'banana', 'cherry']    
+# for fruit in fruits:
+#     print(fruit, end=" ")
+
+# while loop
+i = 0
+while i < 5:
+    print(i, end=" ")
+    i += 1
