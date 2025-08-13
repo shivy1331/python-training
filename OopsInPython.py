18b8ebf791759defbf5be7a3c74d478c79d6b9bf
shivendra singh <shivendrasingh1331@gmail.com>
2025-08-13 11:54:47 +0530

    inheritance

--- OopsInPython.py
+++ OopsInPython.py
@@ -32,5 +32,38 @@
 # student3 = {'name': 'rav', 'age': 19, 'course': 'Mern'}
 # student4 = {'name': 'rak', 'age': 16, 'course': 'Mern'}
 
-# template or class
+# inheritance
+#parent class and child class
+#class ParentClass:
+ #   pass
+#class ChildClass(ParentClass):
+  #  pass
+class Grandfather:
+    def _init_(self):
+        self.hobby = "Reading the news"
+    def speak(self):
+        print("I am Grandfather. Hobby:", self.hobby)
 
+class Father(Grandfather):
+    def _init_(self):
+        super()._init_()
+        self.job = "Engineer"
+    def speak(self):
+        print("I am Father. Job:", self.job)
+
+class Child(Father):
+    def _init_(self):
+        super()._init_()
+        self.school = "ABC School"
+    def speak(self):
+        print("I am Child. School:", self.school)
+
+# Create objects and call methods
+gf = Grandfather()
+gf.speak()
+
+ft = Father()
+ft.speak()
+
+ch = Child()
+ch.speak()
