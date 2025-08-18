97efacfa6c069f954b44a32de26ed16696b10e45
shivendra singh <shivendrasingh1331@gmail.com>
2025-08-18 09:59:35 +0530

    "venv"

---
+++ file2.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return a+b
+
+# def sub(a, b):
+#     print(a-b)
+
+# def mul(a, b):
+#     print(a*b)
--- python_module.py
+++ python_module.py
@@ -1,4 +1,29 @@
 # basic import
-import math
+# import math
 # print(math.factorial(5)); # 120
+
+# print(dir(math)) 
+# ['__doc__', '__loader__', '__name__', '__package__', '__spec__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 'cbrt', 'ceil', 'comb', 'copysign', 'cos', 'cosh', 'degrees', 'dist', 'e', 'erf', 'erfc', 'exp', 'exp2', 'expm1', 'fabs', 'factorial', 'floor', 'fma', 'fmod', 'frexp', 'fsum', 'gamma', 'gcd', 'hypot', 'inf', 'isclose', 'isfinite', 'isinf', 'isnan', 'isqrt', 'lcm', 'ldexp', 'lgamma', 'log', 'log10', 'log1p', 'log2', 'modf', 'nan', 'nextafter', 'perm', 'pi', 'pow', 'prod', 'radians', 'remainder', 'sin', 'sinh', 'sqrt', 'sumprod', 'tan', 'tanh', 'tau', 'trunc', 'ulp']
+
+# x = math
+# print(help(x))
+
+# from math import * # whole module can use
+# print(sqrt(9))
+# print(pi)
+
+# import math as mt
+# print(mt.pi);
+
+# file import from another file 
+# import file2
+# print(file2.add(2,4))
+# print(file2.sub(4,2))
+# print(file2.mul(2,4))
+
+# from file2 import sub
+# print(sub(2,3))
+
+import file2
+print(file2.add(2,8))
 
